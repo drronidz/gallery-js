51295359456d34fac5df378b10cf6430bc34d9f1
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-10-26 00:10:16 +0100

    Complete Open Modal functionality ...

--- app.js
+++ app.js
@@ -9,15 +9,56 @@ function getElement(selection) {
 }
 
 function Gallery(element) {
+  this.container = element
   this.list = [...element.querySelectorAll('.img')]
+
   // target
   this.modal = getElement('.modal');
   this.modalImg = getElement('.main-img')
   this.modalImages = getElement('.modal-images')
+  this.imageName = getElement('.image-name')
   this.closeBtn = getElement('.close-btn')
   this.nextBtn = getElement('.next-btn')
   this.prevBtn = getElement('.prev-btn')
   console.log(this.list)
+
+  // bind functions
+  // this.openModal = this.openModal.bind(this, event.target, this.list)
+  this.container.addEventListener('click', function (event) {
+    if(event.target.classList.contains('img')) {
+      this.openModal(event.target, this.list)
+    }
+    this.openModal()
+  }.bind(this))
+
+  this.closeBtn.addEventListener('click', function (event) {
+    this.closeModal()
+  }.bind(this))
+}
+
+Gallery.prototype.openModal = function (selectedImage, imageList) {
+  console.log('Selected Image :', selectedImage)
+  console.log('Image List :',imageList)
+  this.setMainImage(selectedImage)
+  this.modalImages.innerHTML = imageList.map(function (image) {
+    return ` 
+           <img src=${image.src} 
+                title=${image.title} 
+                data-id=${image.dataset.id}
+                class=${selectedImage.dataset.id === image.dataset.id ? "modal-img selected" : "modal-img"}  
+                alt="image${image.dataset.id}"/>`
+  }).join('')
+  this.modal.classList.add('open')
+}
+
+Gallery.prototype.setMainImage = function (selectedImage) {
+  console.log('Here : ', selectedImage)
+  this.modalImg.src = selectedImage.src
+  this.imageName.textContent = selectedImage.title
+}
+
+Gallery.prototype.closeModal = function () {
+  this.modal.classList.remove('open')
 }
 
 const nature = new Gallery(getElement('.nature'))
