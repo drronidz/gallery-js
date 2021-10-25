6ae86da8566c020dbba419c9fcc638b430f51cae
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2021-10-26 00:49:49 +0100

    Adding Previous & Next Modal functionalities ...

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ function Gallery(element) {
   // target
   this.modal = getElement('.modal');
   this.modalImg = getElement('.main-img')
-  this.modalImages = getElement('.modal-images')
   this.imageName = getElement('.image-name')
+  this.modalImages = getElement('.modal-images')
   this.closeBtn = getElement('.close-btn')
   this.nextBtn = getElement('.next-btn')
   this.prevBtn = getElement('.prev-btn')
@@ -30,13 +30,10 @@ function Gallery(element) {
 
   this.container.addEventListener('click', function (event) {
     if(event.target.classList.contains('img')) {
+      console.log('TARGET : ',event.target)
       this.openModal(event.target, this.list)
     }
-    this.openModal()
   }.bind(this))
-  this.closeBtn.addEventListener('click', this.closeModal)
-  this.nextBtn.addEventListener('click', this.nextImage)
-  this.prevBtn.addEventListener('click', this.prevImage)
 }
 
 Gallery.prototype.openModal = function (selectedImage, imageList) {
@@ -45,17 +42,19 @@ Gallery.prototype.openModal = function (selectedImage, imageList) {
   this.setMainImage(selectedImage)
   this.modalImages.innerHTML = imageList.map(function (image) {
     return ` 
-           <img src=${image.src} 
-                title=${image.title} 
-                data-id=${image.dataset.id}
-                class=${selectedImage.dataset.id === image.dataset.id ? "modal-img selected" : "modal-img"}  
-                alt="image${image.dataset.id}"/>`
+           <img src="${image.src}" 
+                title="${image.title}" 
+                data-id="${image.dataset.id}"
+                class="${selectedImage.dataset.id === image.dataset.id ? "modal-img selected" : "modal-img"}"  
+                alt="Image ${image.dataset.id}"/>`
   }).join('')
   this.modal.classList.add('open')
+  this.closeBtn.addEventListener('click', this.closeModal)
+  this.nextBtn.addEventListener('click', this.nextImage)
+  this.prevBtn.addEventListener('click', this.prevImage)
 }
 
 Gallery.prototype.setMainImage = function (selectedImage) {
-  console.log('Here : ', selectedImage)
   this.modalImg.src = selectedImage.src
   this.imageName.textContent = selectedImage.title
 }
@@ -68,11 +67,19 @@ Gallery.prototype.closeModal = function () {
 }
 
 Gallery.prototype.nextImage = function () {
-
+  const selected = this.modalImages.querySelector('.selected')
+  const next = selected.nextElementSibling || this.modalImages.firstElementChild
+  selected.classList.remove('selected')
+  next.classList.add('selected')
+  this.setMainImage(next)
 }
 
 Gallery.prototype.prevImage = function () {
-
+  const selected = this.modalImages.querySelector('.selected')
+  const prev = selected.previousElementSibling || this.modalImages.firstElementChild
+  selected.classList.remove('selected')
+  prev.classList.add('selected')
+  this.setMainImage(prev)
 }
 
 const nature = new Gallery(getElement('.nature'))
